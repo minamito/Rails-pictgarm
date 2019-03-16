f8037dc714afad928d10709aad409a6e057b6ce1
南智哉 <techboostminami@gmail.com>
2019-03-16 09:24:06 +0900

    rails07課題

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'mini_magick', '~> 4.8'
 gem "jquery-rails"
 gem 'bcrypt', '~> 3.1.11'
-
+gem "carrierwave"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true
   has_secure_password
+
+  has_many :topics
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
