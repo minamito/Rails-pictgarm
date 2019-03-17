b83233045fb3cddc1ab08e2a0bc2a829ffa3b9dc
南智哉 <techboostminami@gmail.com>
2019-03-17 14:59:08 +0900

    rails06課題修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
   def size_range
-    10.megabytes
+    1..10.megabytes
   end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+
 end
